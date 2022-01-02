1eb75b27076055bbd26dfe8b5c187ae7ca39184f
Charlie9830 <charliewjhall@gmail.com>
2022-01-02 20:37:42 +1100

    ManifestModel showId property

--- lib/models/ManifestModel.dart
+++ lib/models/ManifestModel.dart
@@ -6,6 +6,9 @@ class ManifestModel {
   final String
       validationKey; // Used by the storage validation to ensure we are validating against an actual manifest, not just
   // some other random json file.
+  final String
+      showId; // showId is only set when a show is saved for the first time. We will likely use it to assist
+  //  with on Player device preset mergers.
   final String fileName;
   final String created;
   final String modified;
@@ -15,6 +18,7 @@ class ManifestModel {
 
   ManifestModel({
     this.validationKey = '',
+    this.showId = '',
     this.fileName = '',
     this.created = '',
     this.modified = '',
@@ -26,6 +30,7 @@ class ManifestModel {
   Map<String, dynamic> toMap() {
     return {
       'validationKey': manifestModelValidationKeyValue,
+      'showId': showId,
       'fileName': fileName,
       'created': created,
       'modified': modified,
@@ -38,6 +43,7 @@ class ManifestModel {
   factory ManifestModel.fromMap(Map<String, dynamic> map) {
     return ManifestModel(
       validationKey: map['validationKey'] ?? '',
+      showId: map['showId'] ?? '',
       fileName: map['fileName'] ?? '',
       created: map['created'] ?? '',
       modified: map['modified'] ?? '',
@@ -51,6 +57,7 @@ class ManifestModel {
 
   ManifestModel copyWith({
     String? fileName,
+    String? showId,
     String? created,
     String? modified,
     String? createdByVersion,
@@ -60,6 +67,7 @@ class ManifestModel {
     return ManifestModel(
       validationKey: validationKey,
       fileName: fileName ?? this.fileName,
+      showId: showId ?? this.showId,
       created: created ?? this.created,
       modified: modified ?? this.modified,
       createdByVersion: createdByVersion ?? this.createdByVersion,

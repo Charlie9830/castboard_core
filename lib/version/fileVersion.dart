d481770cff049139d129f33783f40d3575e6f9c9
Charlie9830 <charliewjhall@gmail.com>
2022-06-25 18:32:03 +1000

    Documentation

--- lib/version/fileVersion.dart
+++ lib/version/fileVersion.dart
@@ -1,1 +1,6 @@
+///
+///     IMPORTANT
+///
+/// If you are enumerating this value, ensure you add a corresponsing version migration function to the [ImportedShowData] class,
+/// even if it is just a NOOP style function. Otherwise you can end up breaking the Cascade Migration logic in the [ensureMigrated] method.
 const int kMaxAllowedFileVersion = 2;

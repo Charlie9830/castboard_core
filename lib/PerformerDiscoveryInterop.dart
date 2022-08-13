8c4c9250da7983b5a1696fd6d0f9569cdcdcf267
Charlie Hall <charliewjhall@gmail.com>
2022-08-13 17:08:52 +1000

    Linux Avahi implementation

--- lib/PerformerDiscoveryInterop.dart
+++ lib/PerformerDiscoveryInterop.dart
@@ -6,8 +6,11 @@ import 'package:collection/collection.dart';
 const String multicastAddress = '224.101.101.101';
 const int discoveryPort = 8030;
 const int unicastConnectivityPort = 8031;
-final discoveryMagicBytes = Uint8List.fromList([77, 69, 79, 87, 84]); // Utf8 encoding of "MEOWT"
+final discoveryMagicBytes =
+    Uint8List.fromList([77, 69, 79, 87, 84]); // Utf8 encoding of "MEOWT"
 final discoveryReplyPayload = utf8.encode("castboard_performer");
+
+const String kMdnsDeviceNamePrefix = "performer-";
 
 bool hasMagicBytes(Uint8List other) {
   final header = other.take(discoveryMagicBytes.length);

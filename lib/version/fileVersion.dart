d0d7d6763fadc8f15cc1570743d3223f92c73f9b
Charlie9830 <charliewjhall@gmail.com>
2022-11-02 21:07:30 +1100

    Typo

--- lib/version/fileVersion.dart
+++ lib/version/fileVersion.dart
@@ -2,6 +2,6 @@
 ///     IMPORTANT
 ///
 /// If you are enumerating this value, ensure you add a corresponsing version migration function to the
-///  /lib/storage.showfile_migration functions.
+///  /lib/storage/showfile_migration functions.
 /// Even if it is just a NOOP style function. Otherwise you can end up breaking the Cascade Migration logic in the [ensureMigrated] method.
 const int kMaxAllowedFileVersion = 3;

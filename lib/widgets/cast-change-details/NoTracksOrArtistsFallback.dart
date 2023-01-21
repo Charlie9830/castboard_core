6892c0b8a4dc951d4beb2d613996690d58772382
Charlie9830 <charliewjhall@gmail.com>
2023-01-21 12:26:22 +1100

    Tidy Ups

--- lib/widgets/cast-change-details/NoTracksOrArtistsFallback.dart
+++ lib/widgets/cast-change-details/NoTracksOrArtistsFallback.dart
@@ -1,5 +1,9 @@
 import 'package:castboard_core/utils/is_mobile_layout.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+const String kMessage =
+    "There are no Cast Changes to display. Please user the designer desktop application to add these to your showfile.";
 
 class NoTracksOrArtistsFallback extends StatelessWidget {
   const NoTracksOrArtistsFallback({Key? key}) : super(key: key);
@@ -7,13 +11,17 @@ class NoTracksOrArtistsFallback extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (isMobileLayout(context)) {
-      return Text(
-          "Uh oh.. There aren't any tracks or artists. Please user the designer desktop application to add these to your showfile.",
-          style: Theme.of(context).textTheme.caption);
+      return Center(
+        child: Text(kMessage, style: Theme.of(context).textTheme.bodySmall),
+      );
+    }
+
+    if (kIsWeb) {
+      return Text(kMessage, style: Theme.of(context).textTheme.bodySmall);
     }
 
     return Text(
         'Tracks and Artists you create will appear here ready to be assigned to eachother.',
-        style: Theme.of(context).textTheme.caption);
+        style: Theme.of(context).textTheme.bodySmall);
   }
 }

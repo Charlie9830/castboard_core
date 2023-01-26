0a94ca7aa3dab7392d5f9d30b7ac95fa10fe46f3
Charlie9830 <charliewjhall@gmail.com>
2023-01-26 20:52:41 +1100

    Fix Discovery unicast porting

--- lib/PerformerDiscoveryInterop.dart
+++ lib/PerformerDiscoveryInterop.dart
@@ -3,9 +3,8 @@ import 'dart:typed_data';
 
 import 'package:collection/collection.dart';
 
-const String multicastAddress = '224.101.101.101';
-const int discoveryPort = 8030;
 const int unicastConnectivityPort = 8031;
+const int ttl = 8;
 final discoveryMagicBytes =
     Uint8List.fromList([77, 69, 79, 87, 84]); // Utf8 encoding of "MEOWT"
 final discoveryReplyPayload = utf8.encode("castboard_performer");

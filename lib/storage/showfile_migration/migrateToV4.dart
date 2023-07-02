192cd6c6bdcdb929d8fc24523823ef3502e6d527
Charlie9830 <charliewjhall@gmail.com>
2023-07-02 14:33:44 +1000

    Docs update

--- lib/storage/showfile_migration/migrateToV4.dart
+++ lib/storage/showfile_migration/migrateToV4.dart
@@ -6,6 +6,9 @@ Future<ImportedShowData> migrateToV4(
   ImportedShowData source,
   Directory baseDir,
 ) async {
-  // V4 Preserves png files when imported as Image Elements. No Migratation behaviour is required, however this breaks compatability with versions prior to file version v4.
+  // v4
+  // -> Preserves png files when imported as Image Elements. No Migratation behaviour is required,
+  //    however this breaks compatability with versions prior to file version v4.
+  // -> Added Subtitles Feature. Uses Null value guarding so no specific migration required.
   return source.copyWith();
 }
